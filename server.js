4abe424ac47def9f157a3b5295f284be2542b59b
Gator <akpojisg@gmatc.matc.edu>
2022-02-13 23:23:53 -0600

    completed actiivity-3

--- server.js
+++ server.js
@@ -1,16 +1,66 @@
 import { decodeBase64 } from 'bcryptjs';
 import express from 'express';
+import res from 'express/lib/response';
 import connectDatabase from './config/db.js';
+import {check, validationResult} from 'express-validator';
+const app = express();
+
+connectDatabase();
+app.get('/', (req, res) => res.send('http get request to root api'));
+
 //import connectDatabase from ''
 
 //import api from '/src/server/api/index.js/';
 //Init express app
-const app = express();
-//API Endpoints
-//();
-connectDatabase();
+/**
+ * @route POST api/users
+ * @desc Register user
+ * 
+ */
+ app.get('/', (req, res) => res.send('http get request to root api'));
+ /**
+  * @route GET /
+  * @desc
+  * 
+  */
+ app.post('/api/users',
+ [
+     check('name', 'please Enter your name').not().isEmpty(),
+     check('email','Please enter your email' ).isEmail(),
+     check('password', 'please enter password containing 6 or more chars').isLength({min :6})
+ 
+ ],
+ (req, res) =>
+ {
+     const errors = validationResult(req);
+     if(!errors.isEmpty())
+     {
+         return res.status(422).json({errors: errors.array()});
+     }
+     else
+     {
+         return res.send(req.body);
+     }
+     console.log(req.body);
+     res.send(req.body);
+ }
+ )
+ //const app = express();
+ //API Endpoints
+ //();
+//app.use(express.json({extended: false}));
+//app.get('/', (req, res) => res.send('http get request sent to root api endpoint'));
+/**
+ *
+ * 
+ * 
+ * 
+ */
+// app.get('/', (req, res) => res.send('http get request to root api'));
+
+
 //testing
 
 
-app.get('/', (req, res) => res.send('http get request to root api'));
+
 app.listen(3000, () => console.log("listening on port 3000"));   
